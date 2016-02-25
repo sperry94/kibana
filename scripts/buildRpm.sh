8fd62f2a235c4748e9aa9f599494aed05ef5767a
Yannick.Polius <Yannick.Polius@logrhythm.com>
2016-02-25 15:19:51 -0700

    buildRpm.sh, install_nvm.sh: added scripts for building kibana

---
+++ scripts/buildRpm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+PACKAGE=kibana
+GIT_VERSION=`git rev-list --branches HEAD | wc -l`
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+VERSION="$GIT_BRANCH.$GIT_VERSION"
+
+PWD=`pwd`
+
+rm -rf ~/rpmbuild
+rpmdev-setuptree
+cp packaging/$PACKAGE.spec ~/rpmbuild/SPECS
+rm -f $PACKAGE-$VERSION.tar.gz
+mkdir -p ~/rpmbuild/SOURCES
+tar cvzf ~/rpmbuild/SOURCES/$PACKAGE-$VERSION.tar.gz -C $PWD .
+rpmbuild -v -bb --define="version ${VERSION}" --define="kibana_version ${GIT_BRANCH}" --target=x86_64 ~/rpmbuild/SPECS/$PACKAGE.spec
