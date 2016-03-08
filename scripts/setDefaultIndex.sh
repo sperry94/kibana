ec4c6ac7d9fd992507a02a91dce670d1aa1d9664
Craig Cogdill <Craig.Cogdill@logrhythm.com>
2016-03-07 17:09:18 -0700

    Better creation and checking on kibana index

--- scripts/setDefaultIndex.sh
+++ scripts/setDefaultIndex.sh
@@ -1,24 +1,34 @@
 #! /bin/sh
 
-XHEAD_PARAMS="-i"
 KIBANA_PREFIX="localhost:9200/.kibana"
+INDEX_PATTERN="/index-pattern/network_*"
+INDEX_PATTERN_SPEC="{\"title\": \"network_*\", \"timeFieldName\": \"@TimeUpdated\", \"customFormats\": {}}"
+DEFAULT_INDEX="{\"defaultIndex\":\"network_*\"}"
+CONFIG="/config/4.1.4"
+
 NOT_FOUND="not found"
-INDEX_PATTERN="{\"title\": \"network_*\", \"timeFieldName\": \"@TimeUpdated\", \"customFormats\": {}}"
-DEFAULT_INDEX="{\"doc\":{\"doc\":{\"defaultIndex\":\"network_*\"},\"defaultIndex\":\"network_*\"}}"
+CREATE="/_create"
 JSON_FLAG="-d"
+XHEAD_PARAMS="-i"
 
 # Create the index pattern and specify TimeUpdated as the default time parameter
-curl -XPOST "$KIBANA_PREFIX"/index-pattern/network_*/?op_type=create -d "$INDEX_PATTERN"
+index_pattern_exists=$(curl "$XHEAD_PARAMS" -XHEAD "$KIBANA_PREFIX""$INDEX_PATTERN" | grep -i "$NOT_FOUND")
+if [ $? == 0  ]; then
+   echo "Index pattern \"network_*\" DOES NOT exist"
+   curl -XPOST "$KIBANA_PREFIX""$INDEX_PATTERN""$CREATE" -d "$INDEX_PATTERN_SPEC"
+else
+   echo "Index pattern \"network_*\" DOES exist"
+fi
 
 # Check to see if the kibana/config index exists already
 #   If it does not, create it before updating it
-config_exists=$(curl "$XHEAD_PARAMS" -XHEAD "$KIBANA_PREFIX"/config | grep -i "$NOT_FOUND")
+config_exists=$(curl "$XHEAD_PARAMS" -XHEAD "$KIBANA_PREFIX""$CONFIG" | grep -i "$NOT_FOUND")
 if [ $? == 0  ]; then
    echo "Config record DOES NOT exist"
-   curl -XPUT "$KIBANA_PREFIX"/config/4.1.4/_create "$JSON_FLAG" "$DEFAULT_INDEX"
+   curl -XPUT "$KIBANA_PREFIX""$CONFIG""$CREATE" "$JSON_FLAG" "$DEFAULT_INDEX"
 else
    echo "Config record DOES exist"
 fi
 
 # Update the config so that the network_* index pattern is set as the default index
-curl -XPOST "$KIBANA_PREFIX"/config/4.1.4/_update "$JSON_FLAG" "$DEFAULT_INDEX"
+#curl -XPOST "$KIBANA_PREFIX"/config/4.1.4/_update "$JSON_FLAG" "$DEFAULT_INDEX"
