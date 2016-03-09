c57832b65bb9ed70814dc073198dd0eeaa5012f7
Craig Cogdill <Craig.Cogdill@logrhythm.com>
2016-03-09 16:15:14 -0700

    Better logic around when we try to update the config/4.1.4 record

--- scripts/loadAssets.sh
+++ scripts/loadAssets.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
+KIBANA_LOG_FILE="/var/log/probe/KibanaStartup.log"
 KIBANA_PREFIX="localhost:9200/.kibana"
 VER="4.1.4"
+
+TOP_LEVEL_DIR="/usr/local/kibana-$VER-linux-x64"
 DASHBOARDS="dashboards"
 VISUALIZATIONS="visualizations"
 SEARCHES="searches"
-
-TOP_LEVEL_DIR="/usr/local/kibana-$VER-linux-x64"
+PRETTY="?pretty"
 
 # Directories directly under the kibana folder include:
 #   dashboards
@@ -20,11 +22,9 @@ for file in $TOP_LEVEL_DIR/$DASHBOARDS/*.json
 do
     FILENAME=${file##*/}
     KIBANA_ID=${FILENAME%.*}
-    echo "Filename is $FILENAME:"
-    echo "  KIBANA_ID is  $KIBANA_ID:"
+    echo `date +'%D %T'` "Creating $FILENAME dashboard record at $KIBANA_PREFIX/dashboard/$KIBANA_ID"         >> $KIBANA_LOG_FILE 
     curl -XPOST ${KIBANA_PREFIX}/dashboard/${KIBANA_ID} \
-         -d @${file} || exit 1
-    echo
+         -d @${file} || exit 1                                                                >> $KIBANA_LOG_FILE
 done
 
 # VISUALIZATIONS
@@ -32,11 +32,9 @@ for file in $TOP_LEVEL_DIR/$VISUALIZATIONS/*.json
 do
     FILENAME=${file##*/}
     KIBANA_ID=${FILENAME%.*}
-    echo "Loading visualization $FILENAME:"
-    echo "  KIBANA_ID is  $KIBANA_ID:"
+    echo `date +'%D %T'` "Creating $FILENAME visualization record at $KIBANA_PREFIX/visualization/$KIBANA_ID" >> $KIBANA_LOG_FILE
     curl -XPOST ${KIBANA_PREFIX}/visualization/${KIBANA_ID} \
-         -d @${file} || exit 1
-    echo
+         -d @${file} || exit 1                                                                >> $KIBANA_LOG_FILE
 done
 
 # SEARCHES
@@ -44,9 +42,7 @@ for file in $TOP_LEVEL_DIR/$SEARCHES/*.json
 do
     FILENAME=${file##*/}
     KIBANA_ID=${FILENAME%.*}
-    echo "Loading search $FILENAME:"
-    echo "  KIBANA_ID is  $KIBANA_ID:"
+    echo `date +'%D %T'` "Creating $FILENAME search record at $KIBANA_PREFIX/search/$KIBANA_ID"               >> $KIBANA_LOG_FILE
     curl -XPOST ${KIBANA_PREFIX}/${KIBANA_INDEX}/search/${KIBANA_ID} \
-        -d @${file} || exit 1
-    echo
+        -d @${file} || exit 1                                                                 >> $KIBANA_LOG_FILE
 done
--- scripts/setDefaultIndex.sh
+++ scripts/setDefaultIndex.sh
@@ -51,14 +51,16 @@ fi
 #  config type with ID 4.1.4 on its own. We need to check for this and update the record
 #  to include "defaultIndex": "network_*"
 
-curl -XGET "$KIBANA_PREFIX""$CONFIG" | grep -i "\"defaultIndex\":\"network_\*\""
+curl -XGET "$KIBANA_PREFIX""$CONFIG" | grep -i '\"defaultIndex\":\"network_\*\"'
 config_has_default_index=$?
-if [ config_has_default_index != 0 ]; then
+if [ $config_has_default_index != 0 ]; then
    echo `date +'%D %T'` "    Config for $VER exists, but no default index is specified"      >> $KIBANA_LOG_FILE
    echo `date +'%D %T'` "    Updating config/$VER record with default index \"network_*\""   >> $KIBANA_LOG_FILE
-   curl -XPOST "$KIBANA_PREFIX""$CONFIG""$UPDATE" "$JSON_FLAG" "$DEFAULT_INDEX"              >> $KIBANA_LOG_FILE
+   curl -XPOST "$KIBANA_PREFIX""$CONFIG""$UPDATE" "$JSON_FLAG" \'{\"doc\": $DEFAULT_INDEX}\' >> $KIBANA_LOG_FILE
    echo `date +'%D %T'` "    Result of -> curl -XGET $KIBANA_PREFIX$CONFIG$PRETTY:"          >> $KIBANA_LOG_FILE
    curl -XGET "$KIBANA_PREFIX""$CONFIG""$PRETTY"                                             >> $KIBANA_LOG_FILE
+else
+   echo `date +'%D %T'` "    defaultIndex parameter for index pattern \"network_*\" is set"  >> $KIBANA_LOG_FILE
 fi
 
 
