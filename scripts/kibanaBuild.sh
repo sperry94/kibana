cd1a7d1d5e29e72c6362e73eadacfc7afb61eaec
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2016-03-14 11:32:02 -0600

    merge issues

--- scripts/kibanaBuild.sh
+++ scripts/kibanaBuild.sh
@@ -44,6 +44,9 @@ rm -rf Protobuffers
 #nvm install "$(cat .node-version)"
 #npm install
 #source nvm/nvm.sh
+rm -rf node_modules
+tar xvzf node_modules.tgz
+
 rm -rf nvm
 tar xvzf nvm.tgz
 export NVM_DIR=$PWD/nvm
