f41856c3f1af60e985b0b7e3459342bc94332b45
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2016-03-14 12:00:40 -0600

    clarify build steps

--- scripts/kibanaBuild.sh
+++ scripts/kibanaBuild.sh
@@ -44,8 +44,16 @@ rm -rf Protobuffers
 #nvm install "$(cat .node-version)"
 #npm install
 #source nvm/nvm.sh
+
+# Extract node_modules
 rm -rf node_modules
 tar xvzf node_modules.tgz
+
+# extracting kibana bower components
+rm -rf src/kibana/bower_components
+tar xvzf bower_components.tgz
+
+# extracting the nvm needed for the final build step
 rm -rf nvm
 tar xvzf nvm.tgz
 export NVM_DIR=$PWD/nvm
