07fa4619f82f48c39db2c9c95b2e12d3e18e61f3
Craig Cogdill <Craig.Cogdill@logrhythm.com>
2016-04-07 10:49:06 -0600

    Dashboard insertions work. Need to mimic the action for viz and searches

--- scripts/loadAssets.py
+++ scripts/loadAssets.py
@@ -14,17 +14,19 @@ logging.basicConfig(filename="/var/log/probe/pythonKibanaStartup.log",
                     datefmt='%Y/%m/%d %H:%M:%S')
 es = Elasticsearch()
 
-resources = "/usr/local/kibana-4.1.4-linux-x64/resources"
+kibana_version = "4.1.4"
+
+resources = "/usr/local/kibana-" + kibana_version + "-linux-x64/resources"
 dashboards_path = resources + "/dashboards" 
 visualizations_path = resources + "/visualizations" 
 searches_path = resources + "/searches" 
-dashboards = [file for file in listdir(dashboards_path) if isfile(join(dashboards_path, file))]
-visualizations = [file for file in listdir(visualizations_path) if isfile(join(visualizations_path, file))]
-searches = [file for file in listdir(searches_path) if isfile(join(searches_path, file))]
 
 dashboard_type = "dashboard"
 visualization_type = "visualization"
 search_type = "search"
+
+kibana_index = ".kibana"
+indent_level = 3
 
 def format_for_update(content):
     return "{ \"doc\": " + str(json.dumps(content)) + " }"
@@ -60,20 +62,27 @@ def update_existing_document(es_index, es_type, es_id, path_to_updated_json):
     delete_document(es_index, es_type, es_id)
     create_document_from_file(es_index, es_type, es_id, path_to_updated_json)
 
+def get_request_as_json(es_index, es_type, es_id):
+    return json.loads(json.dumps(es.get(index=es_index, doc_type=es_type, id=es_id)))
+
+
 def load_assets(es_index, es_type, path_to_files, files):
     for file in files:
+        logging.debug("--------- " + file + " ---------")
         full_file_path = path_to_files + "/" + file
         es_id = get_es_id(file)
-        document_exists = es.exists(index=es_index, doc_type=es_type, id=es_id)
-        if (document_exists):
-            get_response_json = json.loads(json.dumps(es.get(index=es_index, doc_type=es_type, id=es_id)))
-            if ( get_version_of_file(path_to_files + "/" + file) > get_response_json['_source']['version']):
-                logging.info("Current version for file: " + file + " is higher than existing. Updating it...")
+        if (es.exists(index=es_index, doc_type=es_type, id=es_id)):
+            get_response_json = get_request_as_json(es_index, es_type, es_id)
+            version_of_disk_file = get_version_of_file(full_file_path)
+            version_of_es_file = get_response_json['_source']['version']
+            if (version_of_disk_file > version_of_es_file):
+                logging.info("File \"" + str(file) + "\" is outdated and requires update from version " + str(version_of_es_file) +
+                             " to version " + str(version_of_disk_file) + ". Updating it now...")
                 update_existing_document(es_index, es_type, es_id, full_file_path)
             else:
-                logging.info("Current version for file: " + file + " is NOT higher than existing")
+                logging.info("Current version of file \"" + str(file) + "\" in Elasticsearch is up-to-date. Version: " + str(version_of_es_file))
         else:
-            logging.info("Document doesn't exist in ES currently. Adding it now")
+            logging.info("File \"" + str(file) + "\" doesn't exist in Elasticsearch. Creating it now...")
             create_document_from_file(es_index, es_type, es_id, full_file_path)
 
 
@@ -81,11 +90,15 @@ def load_assets(es_index, es_type, path_to_files, files):
 
 # ----------------- MAIN -----------------
 
+# Create arrays of the filenames in each of the resource dirs
+dashboards = [file for file in listdir(dashboards_path) if isfile(join(dashboards_path, file))]
+visualizations = [file for file in listdir(visualizations_path) if isfile(join(visualizations_path, file))]
+searches = [file for file in listdir(searches_path) if isfile(join(searches_path, file))]
+
+# Load all the artifacts appropriately
 logging.debug("================================== DASHBOARDS ==================================")
 load_assets(kibana_index, dashboard_type, dashboards_path, dashboards)
-logging.debug("================================== VISUALIZATIONS ==================================")
-load_assets(kibana_index, dashboard_type, dashboards_path, dashboards)
-logging.debug("================================== SEARCHES ==================================")
-load_assets(kibana_index, dashboard_type, dashboards_path, dashboards)
-
-
+# logging.debug("================================== VISUALIZATIONS ==================================")
+# load_assets(kibana_index, visualization_type, visualizations_path, visualizations)
+# logging.debug("================================== SEARCHES ==================================")
+# load_assets(kibana_index, search_type, searches_path, searches)
