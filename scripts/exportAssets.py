57ae6505a584adbfa75c61c550982c853f4333f7
Craig Cogdill <Craig.Cogdill@logrhythm.com>
2016-04-25 17:03:28 -0600

    Implemented write to file, ES check and get. Only works for single documents, doesnt do full Dashboard parsing yet

--- scripts/exportAssets.py
+++ scripts/exportAssets.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import argparse
+import json
 from util import ElasticsearchUtil
 from util import Logger
 
@@ -12,6 +13,54 @@ logger = Logger(EXPORT_LOG)
 logging, rotating_handler = logger.configure_and_return_logging()
 
 OUTPUT_DIR = os.path.dirname(os.path.realpath(__file__)) 
+OUTPUT_FILE = "export.json"
+
+INDEX = esUtil.KIBANA_INDEX
+TYPE = None
+ID = None
+
+
+def pretty_format(raw_json_content):
+   json_obj = json.loads(json.dumps(raw_json_content))
+   return json.dumps(json_obj, indent = esUtil.INDENT_LEVEL)
+
+
+def get_asset(es_index, es_type, es_id):
+   ignored, doc_exists = esUtil.function_with_timeout(esUtil.ES_QUERY_TIMEOUT,
+                                           esUtil.document_exists,
+                                             es_index,
+                                             es_type,
+                                             es_id)
+   if doc_exists:
+      success, ret = esUtil.get_document(es_index, es_type, es_id)
+      logging.info("GET RETURNS: " + "\n" + pretty_format(ret))
+      return True, ret
+   else:
+      logging.info("No such thing as " + es_id + " in /" + es_index + "/" + es_type + "/")
+      return False, "Not found"
+   
+def print_error_and_usage(argParser, error):
+   print "Error:  " + error + "\n"
+   print argParser.print_help()
+   sys.exit(2)
+
+def santize_input_args(argParser, args):
+   if len(sys.argv) == 1:
+      print_error_and_usage(argParser, "No arguments supplied.")
+   if (args.dash_name is None
+      and args.viz_name is None
+      and args.search_name is None):
+      print_error_and_usage(argParser, "Must have one of the following flags: -d -v -s")
+
+def print_to_file(content, filename=OUTPUT_FILE):
+   with open(filename, 'w') as outputfile:
+      json.dump(content, outputfile, indent=esUtil.INDENT_LEVEL)
+
+def strip_metadata(json_str):
+   ob = json.loads(json.dumps(json_str))
+   print "---- \n " + pretty_format(esUtil.safe_list_read(ob, '_source'))
+   return esUtil.safe_list_read(ob, '_source')
+
 
 # ----------------- MAIN -----------------
 def main(argv):
@@ -22,19 +71,44 @@ def main(argv):
    argParser.add_argument("-s", "--search", dest="search_name", metavar="SEARCH_NAME", help="Export a single search")
    argParser.add_argument("-o", "--outputdir", dest="directory", metavar="DIR_NAME", help="Specify an output directory for the exported file")
 
-   if len(sys.argv) == 1:
-      print "Error: No arguments supplied \n"
-      print argParser.print_help()
-      sys.exit(2)
+   
+   args = argParser.parse_args()
+
+
+   santize_input_args(argParser, args)
+
+   if args.dash_name:
+      print "dashboard name = " + args.dash_name
+      TYPE = "dashboard"
+      ID = args.dash_name
+   elif args.viz_name:
+      print "visualization name = " + args.viz_name
+      TYPE = "visualization"
+      ID = args.viz_name
+   elif args.search_name:
+      print "search name = " + args.search_name
+      TYPE = "search"
+      ID = args.search_name
+
+   if args.directory:
+      OUTPUT_DIR = args.directory
 
    print "path = " + OUTPUT_DIR
 
-   args = vars(argParser.parse_args())
 
-   for arg in args:
-      print "arg: " + str(arg) + "    val: " + str(args[arg])
+   success, ret = get_asset(INDEX, TYPE, ID)
+   if success:
+      print "SUCCESS\n"
+      print pretty_format(ret)
+      to_file = strip_metadata(ret)
+      print_to_file(to_file)
+   else:
+      print "ERROR:   Did not find any " + TYPE + " named " + ID + " in Elasticsearch."
+      sys.exit(2)
 
-   do_something()
+   # for arg in vars(args):
+   #    print "arg: " + str(arg) + "    val: " + str(getattr(args, arg))
+
 
 
 if __name__ == '__main__':
--- scripts/util.py
+++ scripts/util.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python
 
 
-# Elasticsearch global params
-INDENT_LEVEL = 3
-ES_REQUEST_TIMEOUT = 20
-ES_QUERY_TIMEOUT = 20
-STARTUP_TIMEOUT = 300 # 5 minutes
-LOCALHOST="localhost:9200"
-KIBANA_INDEX = ".kibana"
-INDEX_PATTERN_TYPE = "index-pattern"
-CONFIG_TYPE = "config"
-KIBANA_VERSION = "4.1.4"
-
-
 import json
 import time
 import elasticsearch
-es = elasticsearch.Elasticsearch(max_retries=1, timeout=ES_REQUEST_TIMEOUT)
+
+
 
 
 class Logger:
@@ -49,15 +38,18 @@ class Logger:
 
 
 
-
-
-
-
-
-
-
-
 class ElasticsearchUtil():
+  # Elasticsearch global params
+  INDENT_LEVEL = 3
+  ES_REQUEST_TIMEOUT = 20
+  ES_QUERY_TIMEOUT = 20
+  STARTUP_TIMEOUT = 300 # 5 minutes
+  LOCALHOST="localhost:9200"
+  KIBANA_INDEX = ".kibana"
+  INDEX_PATTERN_TYPE = "index-pattern"
+  CONFIG_TYPE = "config"
+  KIBANA_VERSION = "4.1.4"
+  es = elasticsearch.Elasticsearch(max_retries=1, timeout=ES_REQUEST_TIMEOUT)
 
   LOG_FILE="/var/log/probe/KibanaStartup.log"
   ES_LOGGER = None
@@ -69,7 +61,7 @@ class ElasticsearchUtil():
     self.ES_LOGGER = Logger(self.LOG_FILE) 
     self.logging, self.rotating_handler = self.ES_LOGGER.configure_and_return_logging()
 
-  def safe_list_read(l, idx):
+  def safe_list_read(self, l, idx):
     try:
       value = l[idx]
       return value
@@ -77,85 +69,86 @@ class ElasticsearchUtil():
       self.logging.warning("No element in list for index: " + idx)
       return "" 
 
-      def time_has_run_out(start, curr, max):
-        return curr - start > max
+  def time_has_run_out(self, start, curr, max):
+    return curr - start > max
 
-        def format_for_update(content):
-          return "{ \"doc\": " + str(json.dumps(content)) + " }"
+  def format_for_update(self, content):
+    return "{ \"doc\": " + str(json.dumps(content)) + " }"
 
-          def update_document(es_index, es_type, es_id, content):
-            update_ret_raw = json.dumps(es.update(index=es_index,
-              doc_type=es_type,
-              id=es_id,
-              body=content))
+  def update_document(self, es_index, es_type, es_id, content):
+    update_ret_raw = json.dumps(self.es.update(index=es_index,
+                                          doc_type=es_type,
+                                          id=es_id,
+                                          body=content))
     # There is no true/false return value for an ES update
     return True, update_ret_raw
 
-  def copy_dict_keys(dict):
+  def copy_dict_keys(self, dict):
     return dict.fromkeys(dict.keys(), 0)
 
-  def search_index_and_type(es_index, es_type, query):
-    search_response_raw = json.dumps(es.search(index=es_index,
-     doc_type=es_type,
-     q=query),
-    indent=INDENT_LEVEL)
+  def search_index_and_type(self, es_index, es_type, query):
+    search_response_raw = json.dumps(self.es.search(index=es_index,
+                                                    doc_type=es_type,
+                                                    q=query),
+                                                    indent=self.INDENT_LEVEL)
     search_response_json = json.loads(search_response_raw)
-    hits_json = safe_list_read(search_response_json, 'hits')
-    search_number_of_hits = safe_list_read(hits_json, 'total')
-    return search_number_of_hits
+    hits_json = self.safe_list_read(search_response_json, 'hits')
+    search_number_of_hits = self.safe_list_read(hits_json, 'total')
+    return True, search_number_of_hits
   
   # Elasticsearch communication functions
-  def create_document(es_index, es_type, es_id, es_body):
-    es_create_ret = json.dumps(es.create(index=es_index,
+  def create_document(self, es_index, es_type, es_id, es_body):
+    es_create_ret = json.dumps(self.es.create(index=es_index,
       doc_type=es_type,
       id=es_id,
       body=es_body),
     indent=INDENT_LEVEL)
     es_create_ret_json = json.loads(es_create_ret)
-    created = safe_list_read(es_create_ret_json, 'created')
+    created = self.safe_list_read(es_create_ret_json, 'created')
     return created, es_create_ret
 
-  def document_exists(es_index, es_type, es_id):
-    exists_ret = es.exists(index=es_index, doc_type=es_type, id=es_id)
+  def document_exists(self, es_index, es_type, es_id):
+    exists_ret = self.es.exists(index=es_index, doc_type=es_type, id=es_id)
     # es.exists returns a true or false. 
     #   Always report that the function was successful,
     #   and leave the result in the second return value
     return True, exists_ret
    
-  def delete_document(es_index, es_type, es_id):
-    es_del_raw = es.delete(index=es_index, doc_type=es_type, id=es_id)
+  def delete_document(self, es_index, es_type, es_id):
+    es_del_raw = self.es.delete(index=es_index, doc_type=es_type, id=es_id)
     es_del_json = json.loads(json.dumps(es_del_raw))
-    if safe_list_read(es_del_json, 'found'):
+    if self.safe_list_read(es_del_json, 'found'):
       return True, es_del_raw
     else:
       return False, es_del_raw
 
-  def get_document(es_index, es_type, es_id):
-    es_get_raw = es.get(index=es_index, doc_type=es_type, id=es_id)
+  def get_document(self, es_index, es_type, es_id):
+    es_get_raw = self.es.get(index=es_index, doc_type=es_type, id=es_id)
     es_get_json = json.loads(json.dumps(es_get_raw))
-    if safe_list_read(es_get_json, 'found'):
+    if self.safe_list_read(es_get_json, 'found'):
       return True, es_get_raw
     else:
       return False, es_get_raw
 
-  def read_json_from_file(filename):
+  def read_json_from_file(self, filename):
     with open(filename) as file_raw:    
       return json.load(file_raw)
 
-  def function_with_timeout(timeout, function, *args):
+  def function_with_timeout(self, timeout, function, *args):
     keep_running = True
     start_time = time.time()
     itr = 1
     func_status = False
     func_ret = "Not yet run"
-    while not time_has_run_out(start_time, time.time(), timeout) and keep_running:
+    while not self.time_has_run_out(start_time, time.time(), timeout) and keep_running:
       self.logging.info("try_with_timeout: Attempting run number " + str(itr) + " of function")
       try:
         func_status, func_ret = function(*args)
       except elasticsearch.TransportError as es1:
         self.logging.info("Caught elasticsearch TransportError exception: Status Code: " + str(es1.status_code) + ". Retrying...")
-        if es1.status_code == 409:
+        if es1.status_code == 409 or es1.status_code == 404:
           # 409 - DocumentAlreadyExistsException
+          # 404 - DocumentNotFound
           func_status = True
         else:
           func_status = False
@@ -169,9 +162,9 @@ class ElasticsearchUtil():
     return False, "Function timed out if it got this far"
 
 
-  def get_request_as_json(es_index, es_type, es_id):
-    found, raw = function_with_timeout(ES_REQUEST_TIMEOUT,
-                                     get_document,
+  def get_request_as_json(self, es_index, es_type, es_id):
+    found, raw = self.function_with_timeout(ES_REQUEST_TIMEOUT,
+                                      self.get_document,
                                          es_index,
                                          es_type,
                                          es_id)
