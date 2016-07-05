334da098a0f135e39cf0b7596ab950ba22b7be74
Alex Weltman <alex.weltman@logrhythm.com>
2016-07-05 14:47:20 -0600

    autocap and logging working for discover and visualize

--- src/kibana/netmon_libs/custom_modules/save_rule/services/searchAuditor.js
+++ src/kibana/netmon_libs/custom_modules/save_rule/services/searchAuditor.js
@@ -1,12 +1,20 @@
 define(function (require) {
     var app = require('modules').get('app/dashboard');
-    app.factory('searchAuditor', function (elasticSearchFields,
-                                           Restangular) {
-        var previousQuery = {
-            query    : "",
-            fromDate : "",
-            toDate   : ""
-        };
+    var _ = require('lodash');
+    var datemath = require('utils/datemath');
+    var moment = require('moment');
+    var angular = require('angular');
+
+    var timefilter = require('components/timefilter/timefilter');
+    app.factory('searchAuditor', function (elasticSearchFields, 
+      Restangular, timefilter) {
+         var FORMAT_STRING = 'YYYY/MM/DD HH:mm:ss';
+
+         var previousQuery = {
+             query    : '',
+             fromDate : '',
+             toDate   : ''
+         };
         var formatQuery = function(query) {
             var _query = _.isArray(query) ? query : [query];
             if (_.isArray(_query)) {
@@ -22,44 +30,58 @@ define(function (require) {
             }
             return _query;
         };
-        return {
-            autoCapQuery : function(query) {
-                return formatQuery(query)[0];
-            },
-            logQuery : function(query, from, to) {
-                // ignore these searches for search auditing
-                if (!from || !to || 
-                    query === "" || 
-                    query === "*") {
-                    return;
-                }
-                var formatString = "YYYY/MM/DD HH:mm:ss";
-                var from = moment(from).format(formatString);
-                var to = moment(to).format(formatString);
-                if (query === previousQuery.query &&
-                    from === previousQuery.fromDate &&
-                    to === previousQuery.toDate) {
-                    return;
-                }
-                previousQuery = {
-                    query    : query,
-                    fromDate : from,
-                    toDate   : to
-                };
-                Restangular
-                    .all('audit/')
-                    .post({
-                        query    : query,
-                        from     : from,
-                        to       : to
-                    })
-                    .then(function(response){
-                        if (response.error){
-                            var error = response.message || "An unknown error occured";
-                            console.log('Search Audit Error:', error);
-                        }
-                    });
+        
+        var logQuery = function(query) {
+            // ignore these searches for search auditing
+            
+            var timeFrom = datemath.parse(timefilter.time.from);
+            var timeTo = datemath.parse(timefilter.time.to);
+                            
+            if (!timeFrom || !timeTo || 
+                query === '' || 
+                query === '*') {
+                return;
             }
+            
+            timeFrom = moment(timeFrom).format(FORMAT_STRING);
+            timeTo = moment(timeTo).format(FORMAT_STRING);
+            
+            if (query === previousQuery.query &&
+                timeFrom === previousQuery.fromDate &&
+                timeTo === previousQuery.toDate) {
+                return;
+            }
+            previousQuery = {
+                query    : query,
+                fromDate : timeFrom,
+                toDate   : timeTo
+            };
+            Restangular
+                .all('audit/')
+                .post({
+                    query    : query,
+                    from     : timeFrom,
+                    to       : timeTo
+                })
+                .then(function(response){
+                    if (response.error){
+                        var error = response.message || 'An unknown error occured';
+                        console.log('Search Audit Error:', error);
+                    }
+                });
+        };
+        
+        return {
+            logAndCapitalize : function(stateObj) {
+                var capitalizedQuery = formatQuery(stateObj.query.query_string.query)[0];
+                logQuery(capitalizedQuery);
+                // seems like we need a deep copy to force angular to reflect the
+                // query change in the view
+                var newState = angular.copy(stateObj);
+                newState.query.query_string.query = capitalizedQuery;
+                return newState;
+            }
+
         };
     });
 });
--- src/kibana/plugins/dashboard/index.js
+++ src/kibana/plugins/dashboard/index.js
@@ -4,6 +4,7 @@ define(function (require) {
   var angular = require('angular');
   var ConfigTemplate = require('utils/config_template');
   var datemath = require('utils/datemath');
+  
   require('directives/config');
   require('components/courier/courier');
   require('components/config/config');
@@ -70,182 +71,171 @@ define(function (require) {
     .directive('dashboardApp', function (Notifier, courier, AppState,
                                             timefilter, kbnUrl, searchAuditor) {
         return {
-          link: function(scope, elem) {
-              elem.on('submit', function() {
-                  // TODO: find what event kibana4 uses for searches to listen instead
-                  scope.state.query =
-                      {
-                          query_string : {
-                              query : searchAuditor.autoCapQuery(scope.state.query['query_string']['query'])
-                          }
-                      };
-                  scope.filterResults();
-                  searchAuditor.logQuery(scope.state.query['query_string']['query'],
-                                         datemath.parse(scope.timefilter.time.from),
-                                         datemath.parse(scope.timefilter.time.to));
-              });
-          },
-        controller: function ($scope, $route, $routeParams, $location, $http, 
-           configFile, Private, getAppState, saveRuleManager, indexPatterns) {
-        indexPatterns.refresh();
-        $scope.saveRuleManager = saveRuleManager;
-        var queryFilter = Private(require('components/filter_bar/query_filter'));
+          controller: function ($scope, $route, $routeParams, $location, $http, 
+             configFile, Private, getAppState, saveRuleManager, indexPatterns) {
+            indexPatterns.refresh();
+            $scope.saveRuleManager = saveRuleManager;
+            var queryFilter = Private(require('components/filter_bar/query_filter'));
 
-        var notify = new Notifier({
-          location: 'Dashboard'
-        });
+            var notify = new Notifier({
+              location: 'Dashboard'
+            });
 
-        var dash = $scope.dash = $route.current.locals.dash;
+            var dash = $scope.dash = $route.current.locals.dash;
 
-        if (dash.timeRestore && dash.timeTo && dash.timeFrom && !getAppState.previouslyStored()) {
-          timefilter.time.to = dash.timeTo;
-          timefilter.time.from = dash.timeFrom;
-        }
+            if (dash.timeRestore && dash.timeTo && dash.timeFrom && !getAppState.previouslyStored()) {
+              timefilter.time.to = dash.timeTo;
+              timefilter.time.from = dash.timeFrom;
+            }
 
-        $scope.$on('$destroy', dash.destroy);
+            $scope.$on('$destroy', dash.destroy);
 
-        var matchQueryFilter = function (filter) {
-            return (filter.query && filter.query.query_string && !filter.meta) ||
-                (filter.range && (filter.range.gte || filter.range.lte) && !filter.meta);
-        };
+            var matchQueryFilter = function (filter) {
+                return (filter.query && filter.query.query_string && !filter.meta) ||
+                    (filter.range && (filter.range.gte || filter.range.lte) && !filter.meta);
+            };
 
-        var extractQueryFromFilters = function (filters) {
-          var filter = _.find(filters, matchQueryFilter);
-          if (filter) return filter.query || filter.range;
-        };
+            var extractQueryFromFilters = function (filters) {
+              var filter = _.find(filters, matchQueryFilter);
+              if (filter) return filter.query || filter.range;
+            };
 
-        var stateDefaults = {
-          title: dash.title,
-          panels: dash.panelsJSON ? JSON.parse(dash.panelsJSON) : [],
-          query: extractQueryFromFilters(dash.searchSource.getOwn('filter')) || {query_string: {query: '*'}},
-          filters: _.reject(dash.searchSource.getOwn('filter'), matchQueryFilter)
-        };
+            var stateDefaults = {
+              title: dash.title,
+              panels: dash.panelsJSON ? JSON.parse(dash.panelsJSON) : [],
+              query: extractQueryFromFilters(dash.searchSource.getOwn('filter')) || {query_string: {query: '*'}},
+              filters: _.reject(dash.searchSource.getOwn('filter'), matchQueryFilter)
+            };
 
-        var $state = $scope.state = new AppState(stateDefaults);
+            var $state = $scope.state = new AppState(stateDefaults);
 
-        $scope.configTemplate = new ConfigTemplate({
-          save: require('text!plugins/dashboard/partials/save_dashboard.html'),
-          load: require('text!plugins/dashboard/partials/load_dashboard.html'),
-          share: require('text!plugins/dashboard/partials/share.html'),
-          pickVis: require('text!plugins/dashboard/partials/pick_visualization.html')
-        });
+            $scope.configTemplate = new ConfigTemplate({
+              save: require('text!plugins/dashboard/partials/save_dashboard.html'),
+              load: require('text!plugins/dashboard/partials/load_dashboard.html'),
+              share: require('text!plugins/dashboard/partials/share.html'),
+              pickVis: require('text!plugins/dashboard/partials/pick_visualization.html')
+            });
 
-        $scope.refresh = _.bindKey(courier, 'fetch');
+            $scope.refresh = _.bindKey(courier, 'fetch');
 
-        timefilter.enabled = true;
-        $scope.timefilter = timefilter;
-        $scope.$listen(timefilter, 'fetch', $scope.refresh);
+            timefilter.enabled = true;
+            $scope.timefilter = timefilter;
+            $scope.$listen(timefilter, 'fetch', $scope.refresh);
 
-        courier.setRootSearchSource(dash.searchSource);
+            courier.setRootSearchSource(dash.searchSource);
 
-        function init() {
-          updateQueryOnRootSource();
+            function init() {
+              updateQueryOnRootSource();
 
-          var docTitle = Private(require('components/doc_title/doc_title'));
-          if (dash.id) {
-            docTitle.change(dash.title);
-          }
-          var params = $location.search();
-          if (params.query){
-              $state.query.query_string.query = params.query;
-          }
-          $scope.$emit('application.load');
-        }
+              var docTitle = Private(require('components/doc_title/doc_title'));
+              if (dash.id) {
+                docTitle.change(dash.title);
+              }
+              var params = $location.search();
+              if (params.query){
+                  $state.query.query_string.query = params.query;
+              }
+              $scope.$emit('application.load');
+            }
 
-        function updateQueryOnRootSource() {
-          var filters = queryFilter.getFilters();
-          if ($state.query) {
-            dash.searchSource.set('filter', _.union(filters, [{
-              query: $state.query
-            }]));
-          } else {
-            dash.searchSource.set('filter', filters);
-          }
-        }
-
-        // update root source when filters update
-        $scope.$listen(queryFilter, 'update', function () {
-          updateQueryOnRootSource();
-          $state.save();
-        });
-
-        // update data when filters fire fetch event
-        $scope.$listen(queryFilter, 'fetch', $scope.refresh);
-
-        $scope.newDashboard = function () {
-          kbnUrl.change('/dashboard', {});
-        };
-
-        $scope.filterResults = function () {
-          updateQueryOnRootSource();
-          $state.save();
-          $scope.refresh();
-        };
-
-        $scope.save = function () {
-          $state.title = dash.id = dash.title;
-          $state.save();
-          dash.panelsJSON = angular.toJson($state.panels);
-          dash.timeFrom = dash.timeRestore ? timefilter.time.from : undefined;
-          dash.timeTo = dash.timeRestore ? timefilter.time.to : undefined;
-
-          dash.save()
-          .then(function (id) {
-            $scope.configTemplate.close('save');
-            if (id) {
-              notify.info('Saved Dashboard as "' + dash.title + '"');
-              if (dash.id !== $routeParams.id) {
-                kbnUrl.change('/dashboard/{{id}}', {id: dash.id});
+            function updateQueryOnRootSource() {
+              var filters = queryFilter.getFilters();
+              if ($state.query) {
+                dash.searchSource.set('filter', _.union(filters, [{
+                  query: $state.query
+                }]));
+              } else {
+                dash.searchSource.set('filter', filters);
               }
             }
-          })
-          .catch(notify.fatal);
-        };
 
-        var pendingVis = _.size($state.panels);
-        $scope.$on('ready:vis', function () {
-          if (pendingVis) pendingVis--;
-          if (pendingVis === 0) {
-            $state.save();
-            $scope.refresh();
-          }
-        });
+            // update root source when filters update
+            $scope.$listen(queryFilter, 'update', function () {
+              updateQueryOnRootSource();
+              $state.save();
+            });
 
-        // listen for notifications from the grid component that changes have
-        // been made, rather than watching the panels deeply
-        $scope.$on('change:vis', function () {
-          $state.save();
-        });
+            // update data when filters fire fetch event
+            $scope.$listen(queryFilter, 'fetch', $scope.refresh);
 
-        // called by the saved-object-finder when a user clicks a vis
-        $scope.addVis = function (hit) {
-          pendingVis++;
-          $state.panels.push({ id: hit.id, type: 'visualization' });
-        };
-
-        $scope.addSearch = function (hit) {
-          pendingVis++;
-          $state.panels.push({ id: hit.id, type: 'search' });
-        };
-        
-        // Setup configurable values for config directive, after objects are initialized
-        $scope.opts = {
-          dashboard: dash,
-          save: $scope.save,
-          addVis: $scope.addVis,
-          addSearch: $scope.addSearch,
-          shareData: function () {
-            return {
-              link: $location.absUrl(),
-              // This sucks, but seems like the cleanest way. Uhg.
-              embed: '<iframe src="' + $location.absUrl().replace('?', '?embed&') +
-                '" height="600" width="800"></iframe>'
+            $scope.newDashboard = function () {
+              kbnUrl.change('/dashboard', {});
             };
-          }
-        };
 
-        init();
-      }
+            $scope.filterResults = function () {
+              
+            
+              $scope.state = searchAuditor.logAndCapitalize($scope.state);
+              
+              updateQueryOnRootSource();
+              $state.save();
+              $scope.refresh();
+            };
+
+            $scope.save = function () {
+              $state.title = dash.id = dash.title;
+              $state.save();
+              dash.panelsJSON = angular.toJson($state.panels);
+              dash.timeFrom = dash.timeRestore ? timefilter.time.from : undefined;
+              dash.timeTo = dash.timeRestore ? timefilter.time.to : undefined;
+
+              dash.save()
+              .then(function (id) {
+                $scope.configTemplate.close('save');
+                if (id) {
+                  notify.info('Saved Dashboard as "' + dash.title + '"');
+                  if (dash.id !== $routeParams.id) {
+                    kbnUrl.change('/dashboard/{{id}}', {id: dash.id});
+                  }
+                }
+              })
+              .catch(notify.fatal);
+            };
+
+            var pendingVis = _.size($state.panels);
+            $scope.$on('ready:vis', function () {
+              if (pendingVis) pendingVis--;
+              if (pendingVis === 0) {
+                $state.save();
+                $scope.refresh();
+              }
+            });
+
+            // listen for notifications from the grid component that changes have
+            // been made, rather than watching the panels deeply
+            $scope.$on('change:vis', function () {
+              $state.save();
+            });
+
+            // called by the saved-object-finder when a user clicks a vis
+            $scope.addVis = function (hit) {
+              pendingVis++;
+              $state.panels.push({ id: hit.id, type: 'visualization' });
+            };
+
+            $scope.addSearch = function (hit) {
+              pendingVis++;
+              $state.panels.push({ id: hit.id, type: 'search' });
+            };
+            
+            // Setup configurable values for config directive, after objects are initialized
+            $scope.opts = {
+              dashboard: dash,
+              save: $scope.save,
+              addVis: $scope.addVis,
+              addSearch: $scope.addSearch,
+              shareData: function () {
+                return {
+                  link: $location.absUrl(),
+                  // This sucks, but seems like the cleanest way. Uhg.
+                  embed: '<iframe src="' + $location.absUrl().replace('?', '?embed&') +
+                    '" height="600" width="800"></iframe>'
+                };
+              }
+            };
+
+            init();
+          }
     };
   });
   
