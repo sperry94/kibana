d51546fd90a9cb1f859dc1865b956fdbbd75c923
Craig Cogdill <craig.cogdill@logrhythm.com>
2016-09-02 16:35:18 -0600

    Reading the URL from the kibana startup

--- src/kibana/index.js
+++ src/kibana/index.js
@@ -49,6 +49,36 @@ define(function (require) {
          'Pragma': 'no-cache',
          'Expires': 0
       });
+      var fullUrl = window.location;
+      var searchUrl = window.location.search;
+      var hrefUrl = window.location.href;
+
+      console.log("KIBANA: Full Url = **"+ fullUrl +"**");
+      console.log("KIBANA: Search Url = **"+ searchUrl +"**");
+      console.log("KIBANA: Href Url = **"+ hrefUrl +"**");
+      var QueryString = function () {
+        // This function is anonymous, is executed immediately and 
+        // the return value is assigned to QueryString!
+        var query_string = {};
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+          var pair = vars[i].split("=");
+          // If first entry with this name
+          if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+          // If second entry with this name
+          } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+          } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+          }
+        } 
+        return query_string;
+      }();
+      console.log("OFFICIAL JWT: **" + QueryString.token + "**");
       RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
          // TODO: standardized error handling - right now we soft fail errors to empty data
       
